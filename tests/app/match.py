95bc4916c6d5270473a082f59f33340459a893c8
Grecia Cortés <greciamaria@yahoo.com>
2014-09-18 10:24:04 -0500

    Corrección de formato de codigos python

--- tests/app/match.py
+++ tests/app/match.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 class Match:
-    def __init__(self,player1,player2,pacted_sets):
+
+    def __init__(self, player1, player2, pacted_sets):
         self.p1 = player1
         self.p2 = player2
         self.pacted_sets = pacted_sets
